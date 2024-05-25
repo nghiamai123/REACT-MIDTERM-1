fe63b4c0a5253dea11c69fd6c94e00ae0c1f2614
nghiamai <nghiamaiquangtri@gmail.com>
2024-05-25 09:57:14 +0700

    change id and done ex 3

--- Start code/exercise-4/src/components/Place.jsx
+++ Start code/exercise-4/src/components/Place.jsx
@@ -1,8 +1,8 @@
 export default function Place(items) {
-  const {title, image} = items;
+  const {key, title, image} = items;
   console.log(image);
   return (
-    <li key="the place id" className="place-item">
+    <li key={key} className="place-item">
       <button>
         <img src={image.src} alt={image.alt} />
         <h3>{title}</h3>
